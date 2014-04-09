6e4ada41b2fdb8f77c7a43b8f4fdb1d0e7f69b57
ungesehn <mastaa@gmail.com>
2014-04-09 10:09:34 +0200

    some fancy slide in animation added

--- src/com/autocompleteactionbar/MainActivity.java
+++ src/com/autocompleteactionbar/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.autocompleteactionbar;
 
 import java.util.ArrayList;
@@ -12,96 +13,108 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Transformation;
+import android.view.animation.TranslateAnimation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		getActionBar().setDisplayOptions(
-				ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
+        getActionBar().setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
 
-		LayoutInflater inflater = (LayoutInflater) this
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		View v = inflater.inflate(R.layout.searchbox, null);
+        View v = inflater.inflate(R.layout.searchbox, null);
 
-		ArrayList<String> sampleEntries = createSampleList();
+        ArrayList<String> sampleEntries = createSampleList();
 
-		final ImageView searchIcon = (ImageView) v
-				.findViewById(R.id.search_icon);
-		final ClearableAutoCompleteTextView searchBox = (ClearableAutoCompleteTextView) v
-				.findViewById(R.id.search_box);
-		searchBox.setAdapter(new DropdownAdapter(this, sampleEntries));
+        final ImageView searchIcon = (ImageView) v
+                .findViewById(R.id.search_icon);
+        final ClearableAutoCompleteTextView searchBox = (ClearableAutoCompleteTextView) v
+                .findViewById(R.id.search_box);
+        searchBox.setAdapter(new DropdownAdapter(this, sampleEntries));
 
-		// start with the text view hidden in the action bar
-		searchBox.setVisibility(View.INVISIBLE);
-		searchIcon.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				toggleSearch(false);
-			}
-		});
+        // start with the text view hidden in the action bar
+        searchBox.setVisibility(View.INVISIBLE);
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleSearch(false);
+            }
+        });
 
-		searchBox.setOnClearListener(new OnClearListener() {
-			@Override
-			public void onClear() {
-				toggleSearch(true);
-			}
-		});
+        searchBox.setOnClearListener(new OnClearListener() {
+            @Override
+            public void onClear() {
+                toggleSearch(true);
+            }
+        });
 
-		searchBox.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				// ...
-			}
+        searchBox.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                // ...
+            }
 
-		});
+        });
 
-		getActionBar().setCustomView(v);
-	}
+        getActionBar().setCustomView(v);
+    }
 
-	private ArrayList<String> createSampleList() {
-		ArrayList<String> samples = new ArrayList<String>();
-		samples.add("Germany");
-		samples.add("France");
-		samples.add("Spain");
-		samples.add("Greece");
-		samples.add("Poland");
-		samples.add("Austria");
-		return samples;
-	}
+    private ArrayList<String> createSampleList() {
+        ArrayList<String> samples = new ArrayList<String>();
+        samples.add("Germany");
+        samples.add("France");
+        samples.add("Spain");
+        samples.add("Greece");
+        samples.add("Poland");
+        samples.add("Austria");
+        return samples;
+    }
 
-	// this toggles between the visibility of the search icon and the search box
-	// to show search icon - reset = true
-	// to show search box - reset = false
-	protected void toggleSearch(boolean reset) {
-		ClearableAutoCompleteTextView searchBox = (ClearableAutoCompleteTextView) findViewById(R.id.search_box);
-		ImageView searchIcon = (ImageView) findViewById(R.id.search_icon);
-		if (reset) {
-			// hide search box and show search icon
-			searchBox.setText("");
-			searchBox.setVisibility(View.GONE);
-			searchIcon.setVisibility(View.VISIBLE);
-			// hide the keyboard
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(searchBox.getWindowToken(), 0);
-		} else {
-			// hide search icon and show search box
-			searchIcon.setVisibility(View.GONE);
-			searchBox.setVisibility(View.VISIBLE);
-			searchBox.requestFocus();
-			// show the keyboard
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.showSoftInput(searchBox, InputMethodManager.SHOW_IMPLICIT);
-		}
+    // this toggles between the visibility of the search icon and the search box
+    // to show search icon - reset = true
+    // to show search box - reset = false
+    protected void toggleSearch(boolean reset) {
+        ClearableAutoCompleteTextView searchBox = (ClearableAutoCompleteTextView) findViewById(R.id.search_box);
+        ImageView searchIcon = (ImageView) findViewById(R.id.search_icon);
+        if (reset) {
+            // hide search box and show search icon
+            searchBox.setText("");
 
-	}
+            searchBox.setVisibility(View.GONE);
+            searchIcon.setVisibility(View.VISIBLE);
+            // hide the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(searchBox.getWindowToken(), 0);
+        } else {
+            // hide search icon and show search box
+            searchIcon.setVisibility(View.GONE);
+            searchBox.setVisibility(View.VISIBLE);
+
+            // add FANCY animation
+            Animation a = AnimationUtils.loadAnimation(this,
+                    R.anim.push_left_in);
+
+            searchBox.setVisibility(View.VISIBLE);
+            searchBox.findViewById(R.id.search_box).startAnimation(a);
+            searchBox.requestFocus();
+            // show the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(searchBox, InputMethodManager.SHOW_IMPLICIT);
+        }
+
+    }
 
 }
--- src/com/autocompleteactionbar/misc/ClearableAutoCompleteTextView.java
+++ src/com/autocompleteactionbar/misc/ClearableAutoCompleteTextView.java
@@ -1,6 +1,9 @@
+
 package com.autocompleteactionbar.misc;
+
 import com.autocompleteactionbar.R;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -9,96 +12,100 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 
 /**
- * sub class of {@link android.widget.AutoCompleteTextView} that includes a clear (dismiss / close) button with
- * a OnClearListener to handle the event of clicking the button
- * based on code from {@link http://www.gubed.net/clearableautocompletetextview}
+ * sub class of {@link android.widget.AutoCompleteTextView} that includes a
+ * clear (dismiss / close) button with a OnClearListener to handle the event of
+ * clicking the button based on code from {@link http
+ * ://www.gubed.net/clearableautocompletetextview}
+ * 
  * @author Michael Derazon
- *
  */
 public class ClearableAutoCompleteTextView extends AutoCompleteTextView {
-	// was the text just cleared?
-	boolean justCleared = false;
+    // was the text just cleared?
+    boolean justCleared = false;
 
-	// if not set otherwise, the default clear listener clears the text in the
-	// text view
-	private OnClearListener defaultClearListener = new OnClearListener() {
+    // if not set otherwise, the default clear listener clears the text in the
+    // text view
+    private OnClearListener defaultClearListener = new OnClearListener() {
 
-		@Override
-		public void onClear() {
-			ClearableAutoCompleteTextView et = ClearableAutoCompleteTextView.this;
-			et.setText("");
-		}
-	};
+        @Override
+        public void onClear() {
+            ClearableAutoCompleteTextView et = ClearableAutoCompleteTextView.this;
+            et.setText("");
+        }
+    };
 
-	private OnClearListener onClearListener = defaultClearListener;
+    private OnClearListener onClearListener = defaultClearListener;
 
-	// The image we defined for the clear button
-	public Drawable imgClearButton = getResources().getDrawable(
-			R.drawable.navigation_cancel);
+    // The image we defined for the clear button
+    public Drawable imgClearButton = getResources().getDrawable(
+            R.drawable.navigation_cancel);
 
-	public interface OnClearListener {
-		void onClear();
-	}
+    /* Required methods, not used in this implementation */
+    public ClearableAutoCompleteTextView(Context context) {
+        super(context);
+        init();
+    }
 
-	/* Required methods, not used in this implementation */
-	public ClearableAutoCompleteTextView(Context context) {
-		super(context);
-		init();
-	}
+    /* Required methods, not used in this implementation */
+    public ClearableAutoCompleteTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
 
-	/* Required methods, not used in this implementation */
-	public ClearableAutoCompleteTextView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init();
-	}
+    /* Required methods, not used in this implementation */
+    public ClearableAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
-	/* Required methods, not used in this implementation */
-	public ClearableAutoCompleteTextView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
+    public interface OnClearListener {
+        void onClear();
+    }
 
-	void init() {
-		// Set the bounds of the button
-		this.setCompoundDrawablesWithIntrinsicBounds(null, null,
-				imgClearButton, null);
+    void init() {
+        // Set the bounds of the button
+        this.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                imgClearButton, null);
 
-		// if the clear button is pressed, fire up the handler. Otherwise do nothing
-		this.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
+        // if the clear button is pressed, fire up the handler. Otherwise do
+        // nothing
+        this.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
-				ClearableAutoCompleteTextView et = ClearableAutoCompleteTextView.this;
+                ClearableAutoCompleteTextView et = ClearableAutoCompleteTextView.this;
 
-				if (et.getCompoundDrawables()[2] == null)
-					return false;
+                if (et.getCompoundDrawables()[2] == null)
+                    return false;
 
-				if (event.getAction() != MotionEvent.ACTION_UP)
-					return false;
+                if (event.getAction() != MotionEvent.ACTION_UP)
+                    return false;
 
-				if (event.getX() > et.getWidth() - et.getPaddingRight()	- imgClearButton.getIntrinsicWidth()) {
-					onClearListener.onClear();
-					justCleared = true;
-				}
-				return false;
-			}
-		});
-	}
+                if (event.getX() > et.getWidth() - et.getPaddingRight()
+                        - imgClearButton.getIntrinsicWidth()) {
+                    onClearListener.onClear();
+                    justCleared = true;
+                }
+                return false;
+            }
+        });
+    }
 
-	public void setImgClearButton(Drawable imgClearButton) {
-		this.imgClearButton = imgClearButton;
-	}
+    public void setImgClearButton(Drawable imgClearButton) {
+        this.imgClearButton = imgClearButton;
+    }
 
-	public void setOnClearListener(final OnClearListener clearListener) {
-		this.onClearListener = clearListener;
-	}
+    public void setOnClearListener(final OnClearListener clearListener) {
+        this.onClearListener = clearListener;
+    }
 
-	public void hideClearButton() {
-		this.setCompoundDrawables(null, null, null, null);
-	}
+    public void hideClearButton() {
+        this.setCompoundDrawables(null, null, null, null);
+    }
 
-	public void showClearButton() {
-		this.setCompoundDrawablesWithIntrinsicBounds(null, null, imgClearButton, null);
-	}
+    public void showClearButton() {
+        this.setCompoundDrawablesWithIntrinsicBounds(null, null, imgClearButton, null);
+    }
+
 
 }
